reorganise calculation and fix "focused"

spectralEIT/bin/calculation.py
@@ -76,7 +76,7 @@ class LightPropagation():
 
 
     ## Shape of the Rabi function a focused or guided strong laser beam
-    def rabi_shape(self,z,w0f):
+    def rabi_shape(self,z,w0f,wavelength):
 
         self.logger.info("Inside rabi_shape")
 
@@ -92,13 +92,13 @@ class LightPropagation():
         shape = np.zeros(len(zet))
         for i,dz in enumerate(zet):
             if dz < 0:
-                shape[i] = self.cell_prop(dz,w0f,"Free Space")# if (dz < 0 or dz > self.par.lcLength) else self.par.prop)
+                shape[i] = self.cell_prop(dz,w0f,"Free Space",wavelength)# if (dz < 0 or dz > self.par.lcLength) else self.par.prop)
                 continue
             if 0 < dz < self.par.lcLength:
-                shape[i] = self.cell_prop(dz,w0f,self.par.prop)
+                shape[i] = self.cell_prop(dz,w0f,self.par.prop,wavelength)
                 continue
             if dz < self.par.lcLength:
-                shape[i] = self.cell_prop(dz,w0f,"Free Space")
+                shape[i] = self.cell_prop(dz,w0f,"Free Space",wavelength)
 
         if type(z) in LIST_TYPES:
             return shape
@@ -109,9 +109,9 @@ class LightPropagation():
 
 
     ##
-    def cell_prop(self,z,w0,prop):
+    def cell_prop(self,z,w0,prop, wavelength):
         if prop == "Free Space":
-            zR = np.pi*w0**2/self.mat.wavelength
+            zR = np.pi*w0**2/wavelength
             wz = w0 * np.sqrt(1+(z/zR)**2)
             return w0**2/wz**2
         elif prop == "Light Cage":
@@ -132,23 +132,23 @@ class LightPropagation():
         
         k0 = getattr(self.materials,self.materials.mat_list[0]).k0
         
-        self.IoutW, self.IinW, self.IoutT, self.IinT, self.TAbs, self.chiShape, self.rabiFunction, self.t, self.z = self._calculate(wavelength, k0, progress_callback)
+        # self.IoutW, self.IinW, self.IoutT, self.IinT, self.TAbs, self.chiShape, self.rabiFunction, self.t, self.z = self._calculate(wavelength, k0, progress_callback)
 
-        self.logger.info("Calculation finished")
+        # self.logger.info("Calculation finished")
 
     ## Propagation of the light through a Cesium cell with or without a waveguide
-    def _calculate(self, wavelength, k0, progress_callback=None):
+    # def _calculate(self, wavelength, k0, progress_callback=None):
         
-        z             = np.linspace(0,self.par.cellLength,self.par.zsteps)
-        rabiFunction  = np.zeros(self.par.zsteps)
-        TAbsFunctions = np.zeros([len(z),self.par.gridSize])
-        chiFunction   = np.zeros([len(z),self.par.gridSize],dtype=complex)
-        TAbs          = np.ones([self.par.gridSize])
-        IinT          = np.array([])
-        IoutT         = np.array([])
-        IinW          = np.array([])
-        IoutW         = np.array([])
-        t             = np.array([])
+        # z             = np.linspace(0,self.par.cellLength,self.par.zsteps)
+        # rabiFunction  = np.zeros(self.par.zsteps)
+        # TAbsFunctions = np.zeros([len(z),self.par.gridSize])
+        # chiFunction   = np.zeros([len(z),self.par.gridSize],dtype=complex)
+        # TAbs          = np.ones([self.par.gridSize])
+        # IinT          = np.array([])
+        # IoutT         = np.array([])
+        # IinW          = np.array([])
+        # IoutW         = np.array([])
+        # t             = np.array([])
 
         width0 = self.par.width0
         if self.par.propType == "focused":
@@ -168,19 +168,19 @@ class LightPropagation():
         if self.par.lightShape == "pulse": # creating a pulse
 
             df = 2*np.max(self.par.f)/self.par.gridSize # frequency sampling
-            t = 1/df*np.arange(-self.par.gridSize/2,self.par.gridSize/2)/self.par.gridSize
+            self.t = 1/df*np.arange(-self.par.gridSize/2,self.par.gridSize/2)/self.par.gridSize
             # print(self.t)
-            dt = 2*np.max(t)/np.size(t)
+            dt = 2*np.max(self.t)/np.size(self.t)
 
             if self.par.type == "EITStandalone":
                 pulseFreq = 0
             else:
                 pulseFreq = self.par.pulseFreq
 
-            E = aux.GaussPulse(t,self.par.dt/(2*np.sqrt(2*np.log(2))),pulseFreq) # get the gauss pulse in the time domain
-            IinT = np.abs(E)**2
+            E = aux.GaussPulse(self.t,self.par.dt/(2*np.sqrt(2*np.log(2))),pulseFreq) # get the gauss pulse in the time domain
+            self.IinT = np.abs(E)**2
             E = np.fft.fftshift(np.fft.fft(E)*dt) # fourier transfer the gauss pulse into the frequency domain
-            IinW = np.abs(E)**2#/np.max(nabs(E)**2)
+            self.IinW = np.abs(E)**2#/np.max(nabs(E)**2)
 
         elif self.par.lightShape == "cw": # create a continous wave
             E = 1
@@ -190,7 +190,7 @@ class LightPropagation():
         self.logger.info("Set the propagation type, current is %s", self.par.propType)
         if self.par.propType == "focused":
             TFunction = 1
-            for i,zStep in enumerate(z):
+            for i,zStep in enumerate(self.z):
                 if self.cancelBool == True:
                     self.reset()
                     return 0
@@ -198,16 +198,16 @@ class LightPropagation():
                     progress_callback.emit(self.text(), int(100*zStep/self.par.cellLength))
                 # get the rabi frequency
                 if "EIT" in self.par.type:
-                    shape = self.rabi_shape(zStep,width0)
+                    shape = self.rabi_shape(zStep,width0,wavelength)
                     rabi = rabi0*shape
                     chi_e = self.chi_select(rabi)
-                    rabiFunction[i] = rabi
+                    self.rabiFunction[i] = rabi
                 else:
                     raise ValueError("Error in LightPropagation: if the light is focused, the propagation type should be EIT!")
                 n = np.sqrt(1 + chi_e) # get the refraction index
-                chiFunction[i]  = chi_e
-                TFunction *= np.exp(1j*self.mat.k0*n*self.dz)
-                TAbsFunctions[i] = np.abs(TFunction)
+                self.chiFunction[i]  = chi_e
+                TFunction *= np.exp(1j*k0*n*self.dz)
+                self.TAbsFunctions[i] = np.abs(TFunction)
                 rabi = rabi0
         elif self.par.propType == "unfocused":
             rabi = rabi0
@@ -215,25 +215,28 @@ class LightPropagation():
                 chi_e = self.chi_select(rabi)
             else:
                 chi_e = self.chi_select()
-            chiShape = chi_e
+            self.chiShape = chi_e
             n = np.sqrt(1 + chi_e)
             TFunction = np.exp(1j*k0*n*self.par.cellLength)
         else:
             raise ValueError("Error in LightPropagation in focusing selection!")
         E *= TFunction
-        TAbs = np.abs(TFunction)
+        self.TAbs = np.abs(TFunction)
 
         # get the intensities
         if self.par.lightShape == "pulse":
-            IoutW = np.abs(E)**2/np.max(IinW)
-            IinW /= np.max(IinW)
-            IoutT = np.abs(np.fft.ifft(E)/dt)**2/np.max(IinT)
-            IinT /= np.max(IinT)
+            self.IoutW = np.abs(E)**2/np.max(self.IinW)
+            self.IinW /= np.max(self.IinW)
+            self.IoutT = np.abs(np.fft.ifft(E)/dt)**2/np.max(self.IinT)
+            self.IinT /= np.max(self.IinT)
         elif self.par.lightShape == "cw":
-            IoutW = np.abs(E)**2
+            self.IoutW = np.abs(E)**2
 
         # return np.array([E, IinW, IinT, TAbs, chiShape, rabiFunction, t, z], dtype=object)
-        return np.array([IoutW, IoutT, IinW, IinT, TAbs, chiShape, rabiFunction, t, z], dtype=object)
+        # if self.par.propType == "focused":
+        #     return np.array([IoutW, IoutT, IinW, IinT, TAbs, rabiFunction, TAbsFunctions, chiFunction, t, z], dtype=object)
+        # elif self.par.propType == "unfocused":
+        #     return np.array([IoutW, IoutT, IinW, IinT, TAbs, chiShape, rabiFunction, t, z], dtype=object)
 
 
     def chi_select(self, rabi=0):
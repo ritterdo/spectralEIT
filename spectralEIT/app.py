e8fb8168cc8a7f71f14711ecaf8114b5eb149493
Nik <dritter@physik.hu-berlin.de>
2023-08-02 18:07:32 +0200

    Add checkup for logs directory

--- spectralEIT/app.py
+++ spectralEIT/app.py
@@ -11,10 +11,11 @@ sys.path += [ui_path]
 os.environ["QT_MAC_WANTS_LAYER"] = "1"
 
 from spectralEIT.ui.main_window import MainWindow
+from spectralEIT.bin.info_windows import showQuestionBox
 
 import spectralEIT.bin.log as log
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from PyQt5.QtCore import QFile, QTextStream
 
 import platform
@@ -58,5 +59,26 @@ def main():
     sys.exit(app.exec())
     # app.exec_()
 
+def check_logs_dir():
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+def check_logs_dir_size():
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk("logs"):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    if total_size > 100000000:
+        result = showQuestionBox("Warning", "Log folder size is over 100MB. Do you want to delete all old logs?.")
+        if result == QMessageBox.Yes:
+            for dirpath, dirnames, filenames in os.walk("logs"):
+                for f in filenames:
+                    fp = os.path.join(dirpath, f)
+                    os.remove(fp)
+        
+
 if __name__ == "__main__":
+    check_logs_dir()
+    check_logs_dir_size()
     main()
